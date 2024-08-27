319b1804e09d0453e5bf469dd0c28be176b5bed1
Felix <febernard@web.de>
2024-08-27 21:49:41 +0200

    ray tracing beginning

--- buff_over.c
+++ buff_over.c
@@ -4,10 +4,13 @@
 int main() {
     char* name = (char*) malloc(12);
     char* cmd = (char*) malloc(60);
+
     printf("Adresse von *Name*: <%d>\n", name);
     printf("Adresse von *cmd*:  <%d>\n", cmd);
     printf("gib deinen Namen ein:\n");
+
     gets(name);
+    
     printf("Hallo %s! Freut mich, dich kennenzulernen\n", name);
     system(cmd);
 
